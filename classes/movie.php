673c4a196979ac5475de1e6f3908d1a9d2904aed
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-01-28 15:11:47 +0100

    classe movie

--- classes/movie.php
+++ classes/movie.php
@@ -1,3 +1,29 @@
 <?php
 
+    class Movie {
+        public $title;
+        public $year;
+        public $time;
+        public $IMDbRating;
+        public $director;
+        public $stars = []
+
+        public function __construct($_title, $_year, $_time, $_IMDbRating, $_director, $_stars) {
+            $this->title = $_title;
+            $this->year = $_year;
+            $this->time = $_time;
+            $this->IMDbRating = $_IMDbRating;
+            $this->director = $_director;
+            $this->stars = $_stars;
+        }
+
+        public function IsABeautifulFilm() {
+            foreach($stars as $star){
+                if($star == "Chuck Norris"){
+                    $this->IMDbRating = "∞";
+                }
+            }
+        }
+    }
+
 ?>
