c5141a41e008fe9877485ab6294bf92e6faa4ce8
Andrei-Gabriel <andrescu.gabriel.andrei@gmail.com>
2022-01-28 15:53:26 +0100

    Errore

--- classes/movie.php
+++ classes/movie.php
@@ -6,22 +6,18 @@
         public $time;
         public $IMDbRating;
         public $director;
-        public $stars = []
 
-        public function __construct($_title, $_year, $_time, $_IMDbRating, $_director, $_stars) {
+        public function __construct($_title, $_year, $_time, $_IMDbRating, $_director) {
             $this->title = $_title;
             $this->year = $_year;
             $this->time = $_time;
             $this->IMDbRating = $_IMDbRating;
             $this->director = $_director;
-            $this->stars = $_stars;
         }
 
         public function IsABeautifulFilm() {
-            foreach($stars as $star){
-                if($star == "Chuck Norris"){
-                    $this->IMDbRating = "∞";
-                }
+            if($this->IMDbRating >= 8;){
+                $this->IMDbRating = $this->IMDbRating. ' BEAUTIFUL';
             }
         }
     }
--- index.php
+++ index.php
@@ -1,3 +1,34 @@
 <?php
 
+    require_once __DIR__.'/classes/movie.php';
+
+    $movies = [
+        new Movie("Gran Torino", 2008, "1h 56m", 8.1, "Clint Eastwood")
+        new Movie("Il fotografo di Mauthausen", 2018, "1h 50m", 6.8, "Mar Targarona")
+        new Movie("V per Vendetta", 2005, "2h 12m", 8.1, "James McTeigue")
+    ];
 ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body style="background-color: grey; color: #fff;">
+    <h1>Movies</h1>
+    <?php foreach($movies as $movie) { ?>
+        <div>
+            <h3><?php echo $movie->title?></h3>
+            <ul>
+                <li>Anno: <?php echo $movie->year?></li>
+                <li>Durata: <?php echo $movie->time?></li>
+                <li>Voto: <?php echo $movie->IsABeautifulFilm()?></li>
+                <li>Regista: <?php echo $movie->director?></li>
+            </ul>
+        </div>
+    <?php } ?>
+</body>
+</html>
